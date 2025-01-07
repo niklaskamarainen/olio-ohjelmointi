5d21a256bd1ab738b7f6ca7bcb6ba2d1a5872d16
niklaskamarainen <niklas.kamarainen@gmail.com>
2025-01-07 14:21:04 +0200

    tehtävän ensimmäinen osa

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -4,6 +4,28 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    srand(static_cast<unsigned int>(time(0)));// tämä by ChatGPT
+
+    int satunnaisluku = rand() % 20 + 1;
+    int arvaus = 0;
+
+    std::cout << "Tietokone arpoi luvun 1-20." << std::endl;
+
+    while(true){
+        std::cout << "Anna arvaus: " << std::endl;
+        std::cin >> arvaus;
+
+        if(arvaus > satunnaisluku){
+            std::cout << "Luku on pienempi." << std::endl;
+        }
+        else if(arvaus < satunnaisluku){
+            std::cout << "Luku on suurempi." << std::endl;
+        }
+        else if(arvaus == satunnaisluku){
+            std::cout << "Oikein!" << std::endl;
+            break;
+        }
+    }
+
     return 0;
 }
