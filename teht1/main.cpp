c4f3ec026290d91534aa02626e3495f8f16ec478
niklaskamarainen <niklas.kamarainen@gmail.com>
2025-01-09 13:26:38 +0200

    tehtava 1 valmis

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
+#include <stdlib.h>
+#include <ctime>
 
 using namespace std;
 
+int game(int maxnum);
+
 int main()
 {
+    int maxnum;
+
+    std::cout << "Anna korkein etsittävä luku: " << std::endl;
+    std::cin >> maxnum;
+
+    int arvausten_maara = game(maxnum);
+
+    std::cout << "Arvauksia tarvittiin: " << arvausten_maara << std::endl;
+
+    return 0;
+}
+
+
+int game(int maxnum){
     srand(static_cast<unsigned int>(time(0)));// tämä by ChatGPT
 
-    int satunnaisluku = rand() % 20 + 1;
+    int satunnaisluku = rand() % maxnum + 1;
     int arvaus = 0;
+    int arvausten_maara = 0;
 
-    std::cout << "Tietokone arpoi luvun 1-20." << std::endl;
+    std::cout << "Tietokone arpoi luvun väliltä 1-" << maxnum << std::endl;
 
     while(true){
         std::cout << "Anna arvaus: " << std::endl;
         std::cin >> arvaus;
+
+        arvausten_maara++;
 
         if(arvaus > satunnaisluku){
             std::cout << "Luku on pienempi." << std::endl;
@@ -27,5 +48,5 @@ int main()
         }
     }
 
-    return 0;
+    return arvausten_maara;
 }
